7e64e288b5d05d8ab487311bbcade48f9ce510ce
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-12-02 11:01:51 +0100

    first commit

---
+++ src/main/java/pl/warkoczewski/UdemyDeepDiveJava/UdemyDeepDiveJavaApplication.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.UdemyDeepDiveJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyDeepDiveJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyDeepDiveJavaApplication.class, args);
+	}
+
+}
