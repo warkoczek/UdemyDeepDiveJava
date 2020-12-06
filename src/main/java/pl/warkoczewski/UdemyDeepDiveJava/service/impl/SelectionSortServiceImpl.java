b532a0b5784a860cf33c5e93ec07f0f1cfb96087
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-12-06 14:29:45 +0100

    refactored SelectionSortServiceImpl sortAsc(). working after a couple of refactors.

--- src/main/java/pl/warkoczewski/UdemyDeepDiveJava/service/impl/SelectionSortServiceImpl.java
+++ src/main/java/pl/warkoczewski/UdemyDeepDiveJava/service/impl/SelectionSortServiceImpl.java
@@ -19,28 +19,32 @@ public class SelectionSortServiceImpl implements SelectionSortService {
     @Override
     public int[] sortAsc() {
         int[] array = numberRepository.getArray();
-        int indexOfLargest = 0;
-        int last = array.length-1;
-        traverse(array, last, indexOfLargest);
+        getSortedArray(array);
         return array;
+
     }
 
-    private void traverse(int[] array, int last, int indexOfLargest) {
-        while(last > 0) {
-            updateIndexOfLargest(array, last,  indexOfLargest);
-            swap(array, last, indexOfLargest);
+    private void getSortedArray(int[] array) {
+        for(int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+            int indexOfLargest = getAndSetIndexOfLargest(array, lastUnsortedIndex);
+            swap(array, lastUnsortedIndex, indexOfLargest);
         }
     }
 
-    private void updateIndexOfLargest(int[] array, int last, int indexOfLargest) {
-        for(int i=1; i <= last; i++){
+    private int getAndSetIndexOfLargest(int[] array, int lastUnsortedIndex) {
+        int indexOfLargest = 0;
+        for(int i = 1; i <= lastUnsortedIndex; i++){
             if(array[i] > array[indexOfLargest]){
                 indexOfLargest = i;
             }
         }
+        return indexOfLargest;
     }
 
     private void swap(int[] array, int lastUnsortedIndex, int indexOfLargest) {
+        if( lastUnsortedIndex == indexOfLargest){
+            return;
+        }
         int temp = array[lastUnsortedIndex];
         array[lastUnsortedIndex] = array[indexOfLargest];
         array[indexOfLargest] = temp;
